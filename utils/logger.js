243d1fb3c57c6acd5cf501bed5cac2f44592e0f2
Warlord-10 <deepanshu10joshi@gmail.com>
2025-02-09 17:14:06 +0530

    added verbose command line argument

--- utils/logger.js
+++ utils/logger.js
@@ -3,11 +3,14 @@ const greenColor = '\x1b[32m';
 const resetColor = '\x1b[0m';
 const yellowColor = "\x1b[33m"
 
+const args = process.argv;
+const isVerbose = args.includes("--verbose");
+
 function logInfo(log){
-    if(process.env.MODE === "dev") console.log(`${greenColor}[INFO]${yellowColor}[${new Date()}]:${resetColor}`, log);
+    if(isVerbose) console.log(`${greenColor}[INFO]${yellowColor}[${new Date()}]:${resetColor}`, log);
 }
 function logError(log){
-    if(process.env.MODE === "dev") console.error(`${redColor}[ERROR]${yellowColor}[${new Date()}]:${resetColor}`, log);
+    if(isVerbose) console.error(`${redColor}[ERROR]${yellowColor}[${new Date()}]:${resetColor}`, log);
 }
 
 module.exports = {logError, logInfo};
